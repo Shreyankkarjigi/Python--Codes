aed9e769a53cec40b6fb657b586efecff76a2552
Shreyankkarjigi <androgeek123@gmail.com>
2020-06-04 22:50:34 +0530

    Create Factorial of a number.py
    Compute factorial of a number

---
+++ Factorial of a number.py
@@ -0,0 +1,34 @@
+#factorial
+#Code by Shreyank #Github-https://github.com/Shreyankkarjigi
+#problem: Wap to display factorial of a number
+
+#logic
+#no factorial for negative number
+#factorial of 0 does not exist
+#factorial of number is 1 to that number 
+n=int(input("enter number:"))
+
+factorial=1
+
+if n<0:
+    print("sorry factorial does not exist")
+
+elif n==0:
+
+    print("factorial of 0 is 1")
+else:
+
+    for i in range(1,n+1):
+        factorial=factorial*i
+    print("factorial of num is",factorial)
+
+
+'''
+output
+
+enter number:7
+factorial of num is 5040
+
+'''
+
+  
