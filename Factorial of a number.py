9dc57f6bf8eb1bd0cb791500a81e0445073b0f5f
Shreyankkarjigi <androgeek123@gmail.com>
2020-06-04 23:00:44 +0530

    Update Factorial of a number.py

--- Factorial of a number.py
+++ Factorial of a number.py
@@ -1,34 +1,10 @@
-#factorial
+#factors
 #Code by Shreyank #Github-https://github.com/Shreyankkarjigi
-#problem: Wap to display factorial of a number
-
-#logic
-#no factorial for negative number
-#factorial of 0 does not exist
-#factorial of number is 1 to that number 
-n=int(input("enter number:"))
-
-factorial=1
-
-if n<0:
-    print("sorry factorial does not exist")
-
-elif n==0:
-
-    print("factorial of 0 is 1")
-else:
-
-    for i in range(1,n+1):
-        factorial=factorial*i
-    print("factorial of num is",factorial)
+#problem: Wap to display factors of a number
 
 
-'''
-output
+n=int(input("enter number:\n"))
 
-enter number:7
-factorial of num is 5040
-
-'''
-
-  
+for i in range(1,n+1):
+    if n%i==0:
+        print(i)
